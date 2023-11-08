d4482684ca34889d692494fc6ea4a1743bdcfec2
arvinpaundra <arvinpaundra06@gmail.com>
2023-11-08 21:18:25 +0700

    feat(course): add dependency injection and routes course

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,7 +2,13 @@ package routes
 
 import (
 	authCtrl "github.com/arvinpaundra/go-eduworld/internal/controllers/auth"
+	courseCtrl "github.com/arvinpaundra/go-eduworld/internal/controllers/course"
+	materialCtrl "github.com/arvinpaundra/go-eduworld/internal/controllers/material"
+	moduleCtrl "github.com/arvinpaundra/go-eduworld/internal/controllers/module"
 	authSrvc "github.com/arvinpaundra/go-eduworld/internal/services/auth"
+	courseSrvc "github.com/arvinpaundra/go-eduworld/internal/services/course"
+	materialSrvc "github.com/arvinpaundra/go-eduworld/internal/services/material"
+	moduleSrvc "github.com/arvinpaundra/go-eduworld/internal/services/module"
 	"github.com/uptrace/bun"
 
 	"github.com/arvinpaundra/go-eduworld/internal/drivers"
@@ -24,6 +30,10 @@ func (c *Config) Start() {
 	sessionRepository := drivers.NewSessionRepository(c.Sql)
 	userRepository := drivers.NewUserRepository(c.Sql)
 	interestRepository := drivers.NewInterestRepository(c.Sql)
+	categoryRepository := drivers.NewCategoryRepository(c.Sql)
+	courseRepository := drivers.NewCourseRepository(c.Sql)
+	moduleRepository := drivers.NewModuleRepository(c.Sql)
+	materialRepository := drivers.NewMaterialRepository(c.Sql)
 	cacheRepository := drivers.NewCacheRepository(c.Redis)
 
 	authService := authSrvc.NewAuthService(
@@ -33,16 +43,68 @@ func (c *Config) Start() {
 		interestRepository,
 		cacheRepository,
 	)
+	courseService := courseSrvc.NewCourseService(
+		userRepository,
+		interestRepository,
+		categoryRepository,
+		courseRepository,
+		moduleRepository,
+		materialRepository,
+		cacheRepository,
+	)
+	moduleService := moduleSrvc.NewModuleService(
+		courseRepository,
+		moduleRepository,
+	)
+	materialService := materialSrvc.NewMaterialService(
+		courseRepository,
+		moduleRepository,
+		materialRepository,
+	)
 
 	authController := authCtrl.NewAuthController(authService)
+	courseController := courseCtrl.NewCourseController(courseService)
+	moduleController := moduleCtrl.NewModuleController(moduleService)
+	materialController := materialCtrl.NewMaterialController(materialService)
 
 	// group app version
 	v1 := c.Fiber.Group("/api/v1")
 
 	// auth routes
 	auth := v1.Group("/auth")
-
 	auth.Post("/login", authController.HandlerLogin)
 	auth.Post("/register", authController.HandlerRegister)
 	auth.Post("/logout", authController.HandlerLogout)
+
+	// course routes
+	course := v1.Group("/courses")
+	course.Get("", courseController.HandlerFindAll)
+	course.Post("", courseController.HandlerCreate)
+
+	courseDetail := course.Group("/:course_id")
+	courseDetail.Get("", courseController.HandlerFindDetail)
+	courseDetail.Put("", courseController.HandlerUpdate)
+	courseDetail.Delete("", courseController.HandlerRemove)
+
+	// module routes
+	module := courseDetail.Group("/modules")
+	module.Post("", moduleController.HandlerCreate)
+
+	moduleDetail := module.Group("/:module_id")
+	moduleDetail.Put("", moduleController.HandlerUpdate)
+	moduleDetail.Delete("", moduleController.HandlerRemove)
+
+	// material routes
+	material := moduleDetail.Group("/materials")
+	material.Post("", materialController.HandlerCreate)
+
+	materialDetail := material.Group("/:material_id")
+	materialDetail.Get("", materialController.HandlerFindOne)
+	materialDetail.Put("", materialController.HandlerUpdate)
+	materialDetail.Delete("", materialController.HandlerRemove)
+
+	// mentor routes
+	mentor := v1.Group("/mentors")
+	mentorDetail := mentor.Group("/:mentor_id")
+	mentorDetail.Get("/courses", courseController.HandlerFindByMentor)
 }
