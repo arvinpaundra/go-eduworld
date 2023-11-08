42857335af1287d4da67c8a4b2a1e5dcb48830f6
arvinpaundra <arvinpaundra06@gmail.com>
2023-11-08 21:18:57 +0700

    chore: add course error message

--- pkg/constants/errors.go
+++ pkg/constants/errors.go
@@ -11,11 +11,16 @@ var (
 	ErrSessionNotFound  = errors.New("session not found")
 	ErrDeviceNotFound   = errors.New("device not found")
 	ErrInterestNotFound = errors.New("interest not found")
+	ErrCourseNotFound   = errors.New("course not found")
+	ErrCategoryNotFound = errors.New("category not found")
+	ErrMaterialNotFound = errors.New("material not found")
+	ErrModuleNotFounnd  = errors.New("module not found")
 	ErrKeyNotFound      = errors.New("key not found")
 
 	ErrUsernameAlreadyTaken = errors.New("username already taken")
 	ErrEmailAlreadyTaken    = errors.New("email already taken")
 
-	ErrPasswordIncorrect = errors.New("password incorrect")
-	ErrCredentialInvalid = errors.New("invalid username or password")
+	ErrPasswordIncorrect  = errors.New("password incorrect")
+	ErrCredentialInvalid  = errors.New("invalid username or password")
+	ErrInvalidSQLOperator = errors.New("invalid sql operator")
 )
